262a813e9871a4b062ce2c6ff11eec1f74b638d5
AkbarFikri <akbarfikriabdillah@gmail.com>
2024-03-20 22:54:20 +0700

    refactor: Implement parse html template for mailer

--- internal/pkg/mailer/mailer.go
+++ internal/pkg/mailer/mailer.go
@@ -1,10 +1,11 @@
 package mailer
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"net/smtp"
 	"os"
-	"strings"
 
 	"github.com/AkbarFikri/BREECE-BE/internal/pkg/model"
 
@@ -20,6 +21,7 @@ type mailer struct {
 	Password string
 	Host     string
 	Port     string
+	template *template.Template
 }
 
 func NewMailer() EmailService {
@@ -27,17 +29,22 @@ func NewMailer() EmailService {
 	password := os.Getenv("EMAIL_PASSWORD")
 	host := os.Getenv("EMAIL_HOST")
 	port := os.Getenv("EMAIL_PORT")
+	t := template.Must(template.ParseGlob("internal/pkg/mailer/template/*.html"))
 	return &mailer{
 		Username: username,
 		Password: password,
 		Host:     host,
 		Port:     port,
+		template: t,
 	}
 }
 
 func (m *mailer) SendOTP(data model.EmailOTP) error {
-	send := strings.ReplaceAll(otp_email, "{{.otp}}", data.Otp)
-	send = strings.ReplaceAll(send, "{{.name}}", data.Name)
+	buffer := new(bytes.Buffer)
+
+	if err := m.template.ExecuteTemplate(buffer, "otp_email.html", data); err != nil {
+		return err
+	}
 
 	mime := "MIME-version: 1.0;\nContent-Type: Text/html; charset=\"iso-8859-1\";\n\n"
 	fromUser := fmt.Sprintf("From: BREECE <%s>\n", m.Username)
@@ -45,16 +52,17 @@ func (m *mailer) SendOTP(data model.EmailOTP) error {
 	subjectEmail := fmt.Sprintf("Subject: %s\n", data.Subject)
 
 	auth := smtp.PlainAuth("", m.Username, m.Password, m.Host)
-	body := []byte(fromUser + toUser + subjectEmail + mime + send)
+	body := []byte(fromUser + toUser + subjectEmail + mime + buffer.String())
 
 	return smtp.SendMail(m.Host+":"+m.Port, auth, m.Username, []string{data.Email}, body)
 }
 
 func (m *mailer) SendNotification(data model.EmailNotification) error {
-	send := strings.ReplaceAll(send_notification, "{{.event-title}}", data.EventTitle)
-	send = strings.ReplaceAll(send, "{{.name}}", data.Name)
-	send = strings.ReplaceAll(send, "{{.event-start}}", data.EventStart)
-	send = strings.ReplaceAll(send, "{{.venue}}", data.Venue)
+	buffer := new(bytes.Buffer)
+
+	if err := m.template.ExecuteTemplate(buffer, "notification_event_email.html", data); err != nil {
+		return err
+	}
 
 	mime := "MIME-version: 1.0;\nContent-Type: Text/html; charset=\"iso-8859-1\";\n\n"
 	fromUser := fmt.Sprintf("From: BREECE <%s>\n", m.Username)
@@ -62,7 +70,7 @@ func (m *mailer) SendNotification(data model.EmailNotification) error {
 	subjectEmail := fmt.Sprintf("Subject: %s\n", data.Subject)
 
 	auth := smtp.PlainAuth("", m.Username, m.Password, m.Host)
-	body := []byte(fromUser + toUser + subjectEmail + mime + send)
+	body := []byte(fromUser + toUser + subjectEmail + mime + buffer.String())
 
 	return smtp.SendMail(m.Host+":"+m.Port, auth, m.Username, []string{data.Email}, body)
 }
